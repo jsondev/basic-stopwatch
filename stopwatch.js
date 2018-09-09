30d452af7a3d518552745aa76c9f9ef18a1e17e1
James Anderson <jsondev1.0@outlook.com>
2018-09-08 20:40:01 -0400

    creating stop and start methods

--- stopwatch.js
+++ stopwatch.js
@@ -1,4 +1,23 @@
 function stopwatch(){
 let startTime, endTime, running, duration = 0;
 
+this.startTime = function (){
+    if(running){
+        throw new Error ('Stopwatch is current running.')
+    }
+    else{
+        running = true;
+        startTime = new Date();
+    }
+};
+
+this.stop = function(){
+    if(!running){
+        running = false;
+        stopTime = new Date();
+    }
+    else{
+        throw new Error ('Stopwatch is currently stopped.')
+    }
+}
 }
