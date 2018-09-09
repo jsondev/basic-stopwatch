669cc92a00c7ac8ee0077df35d5b95e6af7b25a2
James Anderson <jsondev1.0@outlook.com>
2018-09-08 20:56:01 -0400

    finished stopwatch example

--- stopwatch.js
+++ stopwatch.js
@@ -1,27 +1,41 @@
-function stopwatch(){
-let startTime, endTime, running, duration = 0;
+function stopwatch() {
+    let startTime, endTime, running, duration = 0;
 
-this.startTime = function (){
-    if(running){
-        throw new Error ('Stopwatch is current running.')
-    }
-    else{
+    // starts the watch
+    this.startTime = function () {
+        if (running) {
+            throw new Error('Stopwatch is current running.')
+        }
         running = true;
         startTime = new Date();
-    }
-};
 
-this.stop = function(){
-    if(!running){
-        throw new Error ('Stopwatch is currently stopped.')
-        
-    }
-    else{
+    };
+
+    // stops the watch
+    this.stopTime = function () {
+        if (!running) {
+            throw new Error('Stopwatch is currently stopped.')
+        }
         running = false;
         endTime = new Date();
+
+        // calculate seconds
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
     }
-    // calculate seconds
-    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds;
+    // resets the watch
+    this.reset = function () {
+        startTime = null;
+        endTime = null;
+        running = null;
+        duration = 0;
+    }
+// exposes the duration
+    Object.defineProperty(this, 'duration', {
+        get: function () {
+            return duration;
+        }
+    })
 }
-}
+
+let sw = new stopwatch();
